8330574abaa1887d03ac9f3fc14393b4867f50c3
anywherey <2857579766@qq.com>
2023-09-24 23:40:23 +0800

    引入e2e

--- jest.config.ts
+++ jest.config.ts
@@ -3,17 +3,17 @@ module.exports = {
   preset: "ts-jest",
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "test/__coverage__",
+  coverageDirectory: "tests/__coverage__",
   moduleDirectories: ["src","node_modules",],
   moduleFileExtensions: ["tsx","ts","js"],
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/test/setupTests.ts"],
+  setupFilesAfterEnv: ["<rootDir>/tests/setupTests.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
-    "^~test/(.*)$": "<rootDir>/test/$1",
+    "^~test/(.*)$": "<rootDir>/tests/$1",
     "\\.(less|css)$": 'jest-less-loader' // 支持less
   },
-  testMatch: ["<rootDir>/test/__test__/**/*.test.tsx"],
+  testMatch: ["<rootDir>/tests/__test__/**/*.test.tsx"],
   transform: {
     "^.+\\.ts$": ['ts-jest', {
       babel: true,
